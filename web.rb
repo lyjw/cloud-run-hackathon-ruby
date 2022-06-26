e3ca95d198a570d8228f22170506e3c4d96e4a35
Jane Wong <jaynellew@gmail.com>
2022-06-26 22:58:56 +0100

    Update web.rb

--- web.rb
+++ web.rb
@@ -12,9 +12,42 @@ get '/' do
 end
 
 post '/' do
-  puts "Test"
-  puts JSON.parse(request.body.read)
+  intel = JSON.parse(request.body.read)
+  puts intel
 
-  moves = ['F', 'T', 'L', 'R']
-  moves.sample
+  my_href = intel.dig('_links', 'self', 'href')
+  arena = intel.dig('arena', 'state')
+  my_location = arena.dig(my_href)
+
+  # F <- move Forward
+  # R <- turn Right
+  # L <- turn Left
+  # T <- Throw
+
+  if my_location['wasHit']
+    ['F', 'L', 'R'].sample
+  else
+    # Attack
+    my_direction = my_location['direction']
+    case my_direction
+    when 'N'
+      strike_location = 'y'
+      strike_range = (my_location[strike_location]-3...my_location[strike_location])
+    when 'S'
+      strike_location = 'y'
+      strike_range = (my_location[strike_location]+1..my_location[strike_location]+3)
+    when 'E'
+      strike_location = 'x'
+      strike_range = (my_location[strike_location]+1..my_location[strike_location]+3)
+    when 'W'
+      strike_location = 'x'
+      strike_range = (my_location[strike_location]-3...my_location[strike_location])
+    end
+
+    if arena.any? {|enemy| strike_range.include?(enemy[1][strike_location]) }
+      'T'
+    else
+      ['F', 'L', 'R'].sample
+    end
+  end
 end
